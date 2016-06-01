5bbacd886ae0bed75dd341ff55960b17d02b86cb
ahza64 <ahza64@yahoo.com>
2016-05-31 19:19:49 -0700

    making client side correct api calls, but having CORS issues

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect( 'mongodb://localhost/angular_auth' ||
+mongoose.connect( 'mongodb://localhost/SFsecrets' ||
                   process.env.MONGOLAB_URI ||
                   process.env.MONGOHQ_URL );
 
--- public/scripts/controllers/secrets-new.controller.js
+++ public/scripts/controllers/secrets-new.controller.js
@@ -8,22 +8,20 @@ function SecretsNewController ($location, $http) {
 
   function create() {
     console.log("creating a location");
-    var address ="1334 Emerson St, NE Washington DC";
+    var address = vm.secret.location;
 
     $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' +
-    address + '&key=AIzaSyArJRzc4s49N8ikvOh3ziAehwi9SLRDYgE')
+      address + '&key=AIzaSyArJRzc4s49N8ikvOh3ziAehwi9SLRDYgE',{headers : {'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'}})
       .then(function(_results){
+        console.log("lat/lng in secret object?");
         vm.queryResults = _results.data.results;
         vm.geodata = vm.queryResults[0].geometry.location;
-        console.log(vm.geodata);
-        vm.secretMarker.push({id: 86, latitude: vm.geolat, longitude: vm.geolng});
-        vm.geodata = {};
-        vm.geolat = vm.geodata.lat;
-        vm.geolng = vm.geodata.lng;
-        vm.queryResults = {};
-        vm.queryError = {};
-        vm.secretMarker = [];
 
+        vm.secret.latitude = vm.geodata.lat;
+        vm.secret.longitude = vm.geodata.lng;
+
+        // vm.secretMarker = [];
+        // vm.secretMarker.push({id: 86, latitude: vm.geolat, longitude: vm.geolng});
       $http
         .post('/api/secrets', vm.secret)
         .then(onCreateSuccess, onCreateError);
